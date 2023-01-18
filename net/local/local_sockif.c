957a41e35e91ebae0dc3ae9a8128e89207de9cdf
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-01-18 14:42:42 +0900

    include/sys/socket.h: Add SOCK_CTRL to socket type
    SOCK_CTRL is added to provide special control over network drivers
    and daemons. Currently, SOCK_DGRAM and SOCK_STREAM perform this control,
    but these use socket resources. In the case of usersocket in particular,
    this is a waste of the device's limited socket resources.
    
    (cherry picked from commit 277e0b941aca0f749afaf1db806c9c6033a7247c)

--- net/local/local_sockif.c
+++ net/local/local_sockif.c
@@ -193,6 +193,19 @@ static int local_setup(FAR struct socket *psock, int protocol)
         return local_sockif_alloc(psock);
 #endif /* CONFIG_NET_LOCAL_DGRAM */
 
+#if defined(CONFIG_NET_LOCAL_STREAM) || defined(CONFIG_NET_LOCAL_DGRAM)
+      case SOCK_CTRL:
+        if (psock->s_proto == 0 || psock->s_proto == IPPROTO_TCP ||
+            psock->s_proto == IPPROTO_UDP)
+          {
+            /* Allocate and attach the local connection structure */
+
+            return local_sockif_alloc(psock);
+          }
+
+        return -EPROTONOSUPPORT;
+#endif
+
       default:
         return -EPROTONOSUPPORT;
     }
@@ -292,6 +305,7 @@ static int local_bind(FAR struct socket *psock,
 #ifdef CONFIG_NET_LOCAL_DGRAM
       case SOCK_DGRAM:
 #endif
+      case SOCK_CTRL:
         {
           /* Bind the Unix domain connection structure */
 
@@ -543,6 +557,14 @@ static int local_connect(FAR struct socket *psock,
         break;
 #endif /* CONFIG_NET_LOCAL_DGRAM */
 
+#if defined(CONFIG_NET_LOCAL_STREAM) || defined(CONFIG_NET_LOCAL_DGRAM)
+      case SOCK_CTRL:
+        {
+          return -ENOSYS;
+        }
+        break;
+#endif
+
       default:
         return -EBADF;
     }
@@ -666,6 +688,7 @@ static int local_close(FAR struct socket *psock)
 #ifdef CONFIG_NET_LOCAL_DGRAM
       case SOCK_DGRAM:
 #endif
+      case SOCK_CTRL:
         {
           FAR struct local_conn_s *conn = psock->s_conn;
 
